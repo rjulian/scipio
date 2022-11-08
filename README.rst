e17cc3a177513ddb5a87df6195a80b2e50ecc5d4
Richard Julian <richard@rjulian.net>
2022-11-08 12:21:41 -0800

    Initialize click framework using cookiecutter

---
+++ README.rst
@@ -0,0 +1,3 @@
+scipio
+
+CLI tool to undertake cloud reconnaisance and attacks.
---
+++ scipio/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+CLI tool to undertake cloud reconnaisance and attacks.
+
+.. currentmodule:: scipio
+.. moduleauthor:: Richard Julian <richard@rjulian.net>
+"""
+
+from .version import __version__, __release__  # noqa
